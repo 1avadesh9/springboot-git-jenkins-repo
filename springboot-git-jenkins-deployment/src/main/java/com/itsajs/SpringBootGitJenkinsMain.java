c780714809f27b4acfc711e22401dc27dc31e0fd
1avadesh9 <avadesh.jaiswar1@gmail.com>
2025-07-20 14:31:52 +0530

    Added SpringBootGitJenkinsMain.java on 20-JUL-2025, Sunday

---
+++ springboot-git-jenkins-deployment/src/main/java/com/itsajs/SpringBootGitJenkinsMain.java
@@ -0,0 +1,11 @@
+package com.itsajs;
+
+import java.time.LocalDateTime;
+
+public class SpringBootGitJenkinsMain
+{
+   public static void main(String[] args)
+   {
+	 System.out.println("*******SPRINGBOOT GIT JENKINS PROJECT DEPLOYMENT STARTED ON  "+LocalDateTime.now());
+   }
+}
